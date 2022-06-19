37c2e373802b06e2e620b544c7b1e41fcc20d2dd
Erin Ewart <ems1980@hotmail.com>
2022-06-19 03:43:35 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -36,8 +36,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
-
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
